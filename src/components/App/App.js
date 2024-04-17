4ff68d33b5945708a207a91044e3edc6f2b41bd5
Julia Khabibova <julia.khabibova@mail.ru>
2024-04-17 23:00:35 +0300

    todo app

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import TodoList from '../TodoList/TodoList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <TodoList></TodoList>
+      </header>
+    </div>
+  );
+}
+
+export default App;
